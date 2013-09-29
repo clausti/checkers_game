9d1036ff3e5be3e7d346be292479984042067bca
clausti <clausti@gmail.com>
2013-09-28 21:50:54 -0700

    Implement accepting player coords a7 format

--- player_classes.rb
+++ player_classes.rb
@@ -29,17 +29,17 @@ COLUMN_NOTATION = { "a" => 0,
     start_pos = gets.chomp
     raise InputError, "Not valid coordinates" if start_pos.length != 2
     start_pos = convert_player_input_to_coords(start_pos)
-    raise InputError, "Not valid start coordinates" if start_pos.any? {|el| el.nil?}
-    raise InputError, "Not valid start coordinates" unless (start_pos.is_a?(Array) && start_pos.length == 2)
-    raise InputError, "Not valid start coordinates" unless start_pos.all? {|ord| ord >= 0 && ord < 8}
+    raise InputError, "Not valid coordinates" if start_pos.any? {|el| el.nil?}
+    raise InputError, "Not valid coordinates" unless (start_pos.is_a?(Array) && start_pos.length == 2)
+    raise InputError, "Not valid coordinates" unless start_pos.all? {|ord| ord >= 0 && ord < 8}
 
     print "Move to? (format: a7, etc)  "
     end_pos = gets.chomp
     raise InputError, "Not valid coordinates" if end_pos.length != 2
     end_pos = convert_player_input_to_coords(end_pos)
-    raise InputError, "Not valid end coordinates" if end_pos.any? {|el| el.nil?}
-    raise InputError, "Not valid end coordinates" unless (end_pos.is_a?(Array) && end_pos.length == 2)
-    raise InputError, "Not valid end coordinates" unless end_pos.all? {|ord| ord >= 0 && ord < 8}
+    raise InputError, "Not valid coordinates" if end_pos.any? {|el| el.nil?}
+    raise InputError, "Not valid coordinates" unless (end_pos.is_a?(Array) && end_pos.length == 2)
+    raise InputError, "Not valid coordinates" unless end_pos.all? {|ord| ord >= 0 && ord < 8}
 
     [start_pos, end_pos]
   end
